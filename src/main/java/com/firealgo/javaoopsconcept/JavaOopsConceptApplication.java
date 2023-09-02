03affe1b9f87675ef1f0bfcdc2acfbb5d89a3235
Deepak <deepakguptaoptimistic@gmail.com>
2023-09-02 20:54:57 +0100

    First Commit

---
+++ src/main/java/com/firealgo/javaoopsconcept/JavaOopsConceptApplication.java
@@ -0,0 +1,13 @@
+package com.firealgo.javaoopsconcept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOopsConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOopsConceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firealgo/javaoopsconcept/JavaOopsConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firealgo.javaoopsconcept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOopsConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
